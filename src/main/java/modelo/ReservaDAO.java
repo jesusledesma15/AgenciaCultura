f7a1146e4c54e4a93341c5346969b56d7c76df3b
jesusledesma15 <jesusproint998@gmail.com>
2020-05-21 13:27:13 +0200

    Añade a ReservaDAO, metodo insertarReserva()

--- src/main/java/modelo/ReservaDAO.java
+++ src/main/java/modelo/ReservaDAO.java
@@ -7,6 +7,9 @@ package modelo;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 /**
  *
@@ -15,6 +18,40 @@ import java.sql.Connection;
 public class ReservaDAO {
     
       private static final Connection CONEXION = Conexion.getInstance();
-    
+
+       public static void insertarReserva(int idReserva, int idCliente, int idServicio, LocalDate fecha) {
+
+        // Cadena con la consulta parametrizada
+        String sql = "insert into reservas values (?,?,?,?)";
+
+        PreparedStatement prest;
+
+        try {
+            // Preparamos la inserción de datos  mediante un PreparedStatement
+            prest = CONEXION.prepareStatement(sql);
+
+            // Procedemos a indicar los valores que queremos insertar
+            // Usamos los métodos setXXX(indice, valor)
+            // indice indica la posicion del argumento ?, empieza en 1
+            // valor es el dato que queremos insertar
+            prest.setInt(1, idReserva);
+            prest.setInt(2, idCliente);
+            prest.setInt(3, idServicio);
+            prest.setDate(4, java.sql.Date.valueOf(fecha));
+
+
+            // Ejecutamos la sentencia de inserción preparada anteriormente
+            prest.executeUpdate();
+
+            // Cerramos el recurso PreparedStatement 
+            prest.close();
+
+            // La inserción se realizó con éxito, devolvemos filas afectadas
+        } catch (SQLException e) {
+            System.out.println("Problemas durante la inserción de datos en la tabla Reservas");
+            System.out.println(e);
+
+        }
+    }
     
 }
